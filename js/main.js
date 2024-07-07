ed1de80a8a3ef0f43abb908f8427efd05e2e381b
coyancm <coyancm@users.noreply.github.com>
2024-07-07 17:30:09 -0400

    Mobile Nav Fix
    Fixed js code that was stopping mobile nav a hrefs from linking users to pages

--- js/main.js
+++ js/main.js
@@ -246,29 +246,18 @@
 	siteDatePicker();
 
 	// navigation
-	var OnePageNavigation = function() {
-		var navToggler = $('.site-menu-toggle');
-		$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
-			e.preventDefault();
-			var hash = this.hash;
-			$('html, body').animate({
-				'scrollTop': $(hash).offset().top
-			}, 800, 'swing', function(){
-				window.location.hash = hash;
-			});
-
+var OnePageNavigation = function() {
+	var navToggler = $('.site-menu-toggle');
+	$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#']", function(e) {
+		e.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+			'scrollTop': $(hash).offset().top
+		}, 800, 'swing', function(){
+			window.location.hash = hash;
 		});
-
-    // $("#menu li a[href^='#']").on('click', function(e){
-    //   e.preventDefault();
-    //   navToggler.trigger('click');
-    // });
-
-    $('body').on('activate.bs.scrollspy', function () {
-      // console.log('nice');
-      // alert('yay');
-    })
-  };
-  OnePageNavigation();
+	});
+};
+OnePageNavigation();
 
 });
